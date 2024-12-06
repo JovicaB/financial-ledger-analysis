d29d099106439101ed8f030ea7608a926d8624f7
JovicaB <jovicab@gmail.com>
2024-12-06 14:09:43 +0100

    components_izmena_year_index

--- src/components.py
+++ src/components.py
@@ -88,10 +88,16 @@ class ComponentsFR:
         return self._get_aop_value('9005', year_index)
 
 
-class ComponentsLedger:
+## USAGE ##
+# df = pd.read_parquet(r"data\parquet\financial_reports.parquet")
+# # ComponentsFR(df)._get_aop_value('0002', 1)
+# print(ComponentsFR(df).prihod_od_prodaje(1))
 
-    @staticmethod
-    def get_account_data(df: pd.DataFrame, account: str | list, debit_or_credit: str = 'all') -> pd.DataFrame:
+class ComponentsLedger:
+    def __init__(self, data: pd.DataFrame) -> None:
+        self.data = data
+
+    def get_account_data(self, account: str | list, debit_or_credit: str = 'all') -> pd.DataFrame:
         """
         Filter data based on the account(s) and optionally the debit or credit column.
         
@@ -102,6 +108,8 @@ class ComponentsLedger:
         Returns:
         - Filtered DataFrame with only the relevant columns.
         """
+        df = self.data
+
         if debit_or_credit not in ['debit', 'credit', 'all']:
             raise ValueError("The argument 'debit_or_credit' must be 'debit', 'credit', or 'all'.")
 
@@ -123,6 +131,54 @@ class ComponentsLedger:
 
         return filtered_data
 
+    def sum_account_data_by_month(self, account: str | list, debit_or_credit: str = 'all') -> dict:
+        """
+        Summarize data by month and year, optionally filtering by account(s) and debit or credit.
+
+        Parameters:
+        - account: A string or list of account prefixes to filter by.
+        - debit_or_credit: The column to filter by ('debit', 'credit', or 'all' for no filter). Default is 'all'.
+
+        Returns:
+        - Dictionary with 'month-year' as keys and sums as values.
+        """
+        df = self.data.copy()
+
+        if debit_or_credit not in ['debit', 'credit', 'all']:
+            raise ValueError("The argument 'debit_or_credit' must be 'debit', 'credit', or 'all'.")
+
+        df['account'] = df['account'].astype(str)
+
+        if isinstance(account, str):
+            filtered_data = df[df['account'].str.startswith(account)]
+        elif isinstance(account, list):
+            filtered_data = df[df['account'].str.startswith(tuple(account))]
+        else:
+            raise ValueError("The 'account' parameter must be a string or a list of strings.")
+
+        filtered_data['year'] = filtered_data['date'].dt.year
+        filtered_data['month'] = filtered_data['date'].dt.month
+
+        if debit_or_credit == 'debit':
+            filtered_data = filtered_data[['year', 'month', 'debit']]
+        elif debit_or_credit == 'credit':
+            filtered_data = filtered_data[['year', 'month', 'credit']]
+        else:
+            filtered_data = filtered_data[['year', 'month', 'debit', 'credit']]
+
+        cols_to_sum = ['debit', 'credit']
+        cols_in_data = [col for col in cols_to_sum if col in filtered_data.columns]
+        grouped_data = (
+            filtered_data.groupby(['year', 'month'], as_index=False)[cols_in_data]
+            .sum()
+        )
+
+        grouped_data['month_year'] = grouped_data['year'].astype(str) + '-' + grouped_data['month'].astype(str).str.zfill(2)
+
+        result = round(grouped_data.set_index('month_year')[cols_in_data].sum(axis=1)).to_dict()
+
+        return result
+
     @staticmethod
     def get_annual_data(df: pd.DataFrame, year: int):
         return df[df['date'].dt.year == year]
@@ -141,9 +197,6 @@ class ComponentsLedger:
 
 
 
-# df = pd.read_parquet(r"data\parquet\financial_reports.parquet")
-# ComponentsFR(df)._get_aop_value('0002', 1)
-# print(ComponentsFR(df).prosecne_zalihe_robe(1))
 
 
 # data = {
@@ -156,3 +209,22 @@ class ComponentsLedger:
 # df = ComponentsLedger.calculate_percentage_changes_from_100(df, 'debit')
 # print(df)
 
+# USAGE
+# years = [2019, 2020, 2021, 2022, 2023]
+# df_list = []
+
+# for year in years:
+#     df_list.append(pd.read_parquet(f"data/parquet/financial_journal_{year}.parquet"))
+
+# df = pd.concat(df_list, ignore_index=True)
+
+# df['date'] = pd.to_datetime(df['date'])
+# df['date'] = pd.to_datetime(df['date'])
+
+# df = df[~((df['date'].dt.month == 1) & (df['date'].dt.day == 1))]
+# df = df[~(df['account'].str.startswith(('599', '699', '7')))]
+
+# df = df.reset_index(drop=True)
+# # ----------------------------------------------------------------------- #
+# class_inst = ComponentsLedger(df)
+# print(class_inst.sum_account_data_by_month('02', 'credit'))
